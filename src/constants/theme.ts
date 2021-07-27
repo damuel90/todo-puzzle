cfc026b386fdcc45e1e562391b3a0afd68ba5f24
Damuel90 <damuel90@gmail.com>
2021-07-27 06:48:26 -0400

    [ADDED] theme app

---
+++ src/constants/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    primary: 'rgb(92, 188, 118)',
+    background: 'rgb(249, 248, 248)',
+    text: 'rgb(34, 34, 34)',
+    commons: {
+      black: 'rgb(0, 0, 0)',
+      white: 'rgb(255, 255, 255)',
+    },
+  },
+  padding: 20,
+  font: {
+    size: {
+      h1: 18,
+    },
+  },
+};

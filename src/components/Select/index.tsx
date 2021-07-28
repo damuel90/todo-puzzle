d66ce104bc2544fdd19adbbfc16ee16a1a2a3801
Damuel90 <damuel90@gmail.com>
2021-07-28 01:47:40 -0400

    [REFACTORIZED_CODE] reusable selection component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,34 +1,69 @@
 import React from 'react';
-import RNPickerSelect from 'react-native-picker-select';
+import {StyleSheet} from 'react-native';
+import RNPickerSelect, {Item} from 'react-native-picker-select';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {theme} from '../../constants';
 
-export const Select = () => {
+export interface SelectProps {
+  options: Item[];
+  placeholder?: string;
+  value?: Item;
+  onChange: (value: Item, index: number) => void;
+}
+
+export const Select: React.FC<SelectProps> = ({
+  onChange,
+  value,
+  placeholder,
+  options,
+}) => {
   return (
     <RNPickerSelect
-      onValueChange={value => console.log(value)}
+      placeholder={
+        !value && placeholder
+          ? {
+              label: placeholder,
+              key: 'placeholder',
+              value: null,
+              color: theme.colors.placeholder,
+            }
+          : undefined
+      }
+      value={value}
+      itemKey={value?.label}
+      onValueChange={onChange}
       style={{
-        viewContainer: {
-          backgroundColor: theme.colors.background,
-          height: 50,
-          borderRadius: theme.borderRadius / 2,
-          borderWidth: 1,
-          borderStyle: 'solid',
-          borderColor: '#f3f3f3',
-        },
-        iconContainer: {
-          top: 15,
-          right: 20,
-        },
+        viewContainer: styles.viewContainer,
+        iconContainer: styles.iconContainer,
+        inputAndroid: styles.inputAndroid,
       }}
       Icon={() => (
-        <Ionicons name="chevron-down" size={20} color="rgb(140, 140, 140)" />
+        <Ionicons
+          name="chevron-down"
+          size={20}
+          color={theme.colors.placeholder}
+        />
       )}
-      items={[
-        {label: 'Football', value: 'football'},
-        {label: 'Baseball', value: 'baseball'},
-        {label: 'Hockey', value: 'hockey'},
-      ]}
+      items={options}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    backgroundColor: theme.colors.background,
+    height: theme.buttonHeight,
+    borderRadius: theme.borderRadius / 2,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#f3f3f3',
+  },
+  iconContainer: {
+    top: 15,
+    right: 20,
+  },
+  inputAndroid: {
+    color: theme.colors.textInput,
+    fontSize: 14,
+  },
+});

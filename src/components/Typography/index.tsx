3600cecb068232ef0057f46c8fa47fb709ff40ea
Damuel90 <damuel90@gmail.com>
2021-07-28 05:02:42 -0400

    [ADDED] style prop for Typography component

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {TypographyStyled} from './TypographyStyled';
 import {theme} from '../../constants';
+import {StyleProp, TextStyle} from 'react-native';
 
 export interface TypographyProps {
   color?: string;
   size?: number;
   isBold?: boolean;
+  style?: StyleProp<TextStyle>;
 }
 
 export const Typography: React.FC<TypographyProps> = ({
@@ -13,8 +15,11 @@ export const Typography: React.FC<TypographyProps> = ({
   color = theme.colors.text,
   size = theme.font.size.h1,
   isBold = false,
+  style,
 }) => {
   return (
-    <TypographyStyled {...{color, size, isBold}}>{children}</TypographyStyled>
+    <TypographyStyled {...{color, size, isBold, style}}>
+      {children}
+    </TypographyStyled>
   );
 };

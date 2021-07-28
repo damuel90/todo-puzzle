2286c32a8b9d86832ece1977285768c6d79b09cf
Damuel90 <damuel90@gmail.com>
2021-07-28 01:45:14 -0400

    [ADDED] CustomToastProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import {RootStackNavigation} from './navigations';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {store} from './store';
+import {CustomToastProvider} from './components';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <RootStackNavigation />
-      </NavigationContainer>
-    </Provider>
+    <CustomToastProvider>
+      <Provider store={store}>
+        <NavigationContainer>
+          <RootStackNavigation />
+        </NavigationContainer>
+      </Provider>
+    </CustomToastProvider>
   );
 };
 
